c248d21a6540feb694cd36e394cad05e530403cb
WMumei <luongwinter@gmail.com>
2024-05-27 14:25:17 +0700

    Add project files.

---
+++ JewelryProductionOrder/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace JewelryProductionOrder.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
