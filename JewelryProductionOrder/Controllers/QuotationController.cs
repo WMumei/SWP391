88afb6eb9f3b7de799a228dcd88eab077868458d
ngoxuanhanhnhi <hanhnhi10022005@gmail.com>
2024-05-28 16:46:25 +0700

    Add quotation

---
+++ JewelryProductionOrder/Controllers/QuotationController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SWP391.Controllers
+{
+    public class QuotationController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
