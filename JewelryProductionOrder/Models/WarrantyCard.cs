de2a22265aa79fc7bed73a76d7825146df81c700
WMumei <luongwinter@gmail.com>
2024-05-28 14:11:42 +0700

    feat: Update Basic Schemas

--- JewelryProductionOrder/Models/WarrantyCard.cs
+++ JewelryProductionOrder/Models/WarrantyCard.cs
@@ -1,16 +1,20 @@
-﻿namespace JewelryProductionOrder.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace JewelryProductionOrder.Models
 {
     public class WarrantyCard
     {
         public int Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime ExpiredDate { get; set; }
 
         //public int CustomerId { get; set; }
         //[ForeignKey("CustomerId")]
         //public User User { get; set; }
 
-        //public int Jewelry { get; set; }
-        //[ForeignKey("Jewelry")]
-        //public User user { get; set; }
+        public int JewelryId { get; set; }
+        [ForeignKey("JewelryId")]
+        public Jewelry Jewelry { get; set; }
 
         //public int SalesStaffId { get; set; }
         //[ForeignKey("SalesStaffId")]
