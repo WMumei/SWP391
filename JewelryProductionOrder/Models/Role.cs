00625c1ab9a8ba498116dbc452a699308c4f1df1
WMumei <luongwinter@gmail.com>
2024-05-31 15:45:42 +0700

    feat: updatedb

---
+++ JewelryProductionOrder/Models/Role.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace JewelryProductionOrder.Models
+{
+    public class Role
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+
+    }
+}
