f63e46918f29cb4c9c1f6fa2e630d82db01a1734
WMumei <luongwinter@gmail.com>
2024-06-14 15:17:49 +0700

    dev: Add repository pattern

---
+++ JewelryProductionOrder/Repositories/IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using JewelryProductionOrder.Repositories.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Models.Repositories.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {   
+        IProductionRequestRepository ProductionRequest { get; }
+        void Save();
+    }
+}
