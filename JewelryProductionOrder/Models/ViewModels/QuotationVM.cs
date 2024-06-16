4618e7c32787c36fe29ca4f67e4794b4e1466667
ngoxuanhanhnhi <hanhnhi10022005@gmail.com>
2024-06-16 16:48:36 +0700

    AddAsp-for in CreateQuotationForm

--- JewelryProductionOrder/Models/ViewModels/QuotationVM.cs
+++ JewelryProductionOrder/Models/ViewModels/QuotationVM.cs
@@ -5,6 +5,7 @@
         public QuotationRequest QuotationRequest { get; set; }
         public User SaleStaff { get; set; }
         public Jewelry Jewelry { get; set; }
+        public Material Material { get; set; }
         public MaterialSet MaterialSet { get; set; }
     }
 }
