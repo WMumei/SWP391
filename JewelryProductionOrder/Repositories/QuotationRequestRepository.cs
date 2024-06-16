3d83f213b55054df7bcd563aaf78e79fdec55eb1
ngoxuanhanhnhi <hanhnhi10022005@gmail.com>
2024-06-16 15:26:40 +0700

    AddQuotationRequestRepository

---
+++ JewelryProductionOrder/Repositories/QuotationRequestRepository.cs
@@ -0,0 +1,32 @@
+﻿using JewelryProductionOrder.Data;
+using JewelryProductionOrder.Models;
+using JewelryProductionOrder.Repositories.IRepository;
+using Microsoft.EntityFrameworkCore;
+using Models.Repository;
+
+namespace JewelryProductionOrder.Repositories
+{
+    public class QuotationRequestRepository: Repository<QuotationRequest>, IQuotationRequestRepository
+    {
+        private ApplicationDbContext _db;
+        public QuotationRequestRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+        public IEnumerable<QuotationRequest> GetAllWithSaleStaffs()
+        {
+            return dbSet.Include(x => x.SalesStaff).ToList();
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+
+        public void Update(QuotationRequest request)
+        {
+            _db.QuotationRequests.Update(request);
+        }
+    }
+}
+
