03cf891259dbc93c31886ba37defb4acaff0bd34
ngoxuanhanhnhi <hanhnhi10022005@gmail.com>
2024-06-18 11:05:04 +0700

    EditDelete in WarrantyController

--- JewelryProductionOrder/Controllers/WarrantyCardController.cs
+++ JewelryProductionOrder/Controllers/WarrantyCardController.cs
@@ -11,10 +11,10 @@ using System.Security.Claims;
 
 namespace JewelryProductionOrder.Controllers
 {
-    public class WarrantyCardController : Controller
-    {
-        //private readonly ApplicationDbContext _db;
-        //private readonly IWarrantyCardRepository _warrantyCardRepo;
+	public class WarrantyCardController : Controller
+	{
+		//private readonly ApplicationDbContext _db;
+		private readonly IWarrantyCardRepository _warrantyCardRepo;
 		private readonly IUnitOfWork _unitOfWork;
 		private readonly UserManager<User> _userManager;
 
@@ -45,20 +45,66 @@ namespace JewelryProductionOrder.Controllers
 			vm.WarrantyCard.SalesStaffId = userId;
 
 			vm.WarrantyCard.CreatedAt = DateTime.Now;
-			
-			
+
+
 			//vm.QuotationRequest.MaterialSetId = materialSet.Id;
-			vm.WarrantyCard	.JewelryId = vm.Jewelry.Id;
+			vm.WarrantyCard.JewelryId = vm.Jewelry.Id;
 			//vm.QuotationRequest.TotalPrice = vm.QuotationRequest.LaborPrice + materialSet.TotalPrice;
 			_unitOfWork.WarrantyCard.Add(vm.WarrantyCard);
 			_unitOfWork.Save();
 			return RedirectToAction("Index", "Jewelry");
 		}
 		public IActionResult Index()
-        {
-            List<WarrantyCard> objWarrantyCardList = _warrantyCardRepo.GetAll().ToList();
-            return View();
-        }
+		{
+			List<WarrantyCard> objWarrantyCardList = _unitOfWork.WarrantyCard.GetAll().ToList();
+			return View();
+		}
+		public IActionResult Delete(int? id)
+		{
+			WarrantyCard warrantyCardFromDb = _warrantyCardRepo.Get(u => u.Id == id);
+			if (warrantyCardFromDb == null)
+			{
+				return NotFound();
+			}
+			
+			return View(warrantyCardFromDb);
+		}
+		[HttpPost, ActionName("Delete")]
+		public IActionResult DeletePost(int? id)
+		{
+			WarrantyCard warrantyCard = _warrantyCardRepo.Get(u => u.Id == id);
+			if (warrantyCard == null)
+			{
+				return NotFound();
+			}
+			_unitOfWork.WarrantyCard.Remove(warrantyCard);
+			_unitOfWork.Save();
+			TempData["success"] = "Warranty Card is deleted successfully!";
+			return RedirectToAction("Index");
+		}
+		
+		public IActionResult Edit(int? id)
+		{
+			if (id == null || id == 0)
+			{
+				return NotFound();
+			}
+			WarrantyCard? warrantyCardFromDb = _warrantyCardRepo.Get(u => u.Id == id);
+			if (warrantyCardFromDb == null)
+			{ return NotFound(); }
+			return View(warrantyCardFromDb);
+		}
+		[HttpPost]
+		public IActionResult Edit(WarrantyCardVM vm)
+		{
+			if (ModelState.IsValid)
+			{
+				_unitOfWork.WarrantyCard.Update(vm.WarrantyCard);
 
-    }
+				_unitOfWork.Save();
+				return RedirectToAction("Index", "Jewelry");
+			}
+			return View(vm);
+		}
+	}
 }
