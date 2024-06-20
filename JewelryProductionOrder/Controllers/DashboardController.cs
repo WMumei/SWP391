dd6165936141e7e09daf9bb7f2bfe97f75bc5ff7
HoaNguyen1903 <hoanhtse185071@gmail.com>
2024-06-21 00:50:08 +0700

    Added Dashboard Controller and DashboardLists (contain quotation request for revenue, users for customer count, deliveries for total jewelry sold Added hyperlink leading to Dashboard/Index.cshtml in _Layout.cshtml Added style for Dashboard Downloaded icon

---
+++ JewelryProductionOrder/Controllers/DashboardController.cs
@@ -0,0 +1,17 @@
+﻿using JewelryProductionOrder.Data;
+using Microsoft.AspNetCore.Mvc;
+
+public class DashboardController : Controller
+{
+    private readonly ApplicationDbContext _dbContext;
+
+    public DashboardController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ JewelryProductionOrder/Models/DashboardLists.cs
@@ -0,0 +1,9 @@
+﻿using JewelryProductionOrder.Models.ViewModels;
+using JewelryProductionOrder.Models;
+
+public class DashboardLists
+{
+    public List<ProductionRequest> ProductionRequests { get; set; }
+    public List<Delivery> Deliveries { get; set; }
+    public List<QuotationRequest> QuotationRequests { get; set; }
+}
