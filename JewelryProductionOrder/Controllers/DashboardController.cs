73d7d8cd54d16f932bbac2f2eae87a764ed57a58
HoaNguyen1903 <hoanhtse185071@gmail.com>
2024-06-22 18:54:34 +0700

    Revenue Dashboard done

--- JewelryProductionOrder/Controllers/DashboardController.cs
+++ JewelryProductionOrder/Controllers/DashboardController.cs
@@ -1,28 +1,54 @@
-﻿using JewelryProductionOrder.Data;
-using JewelryProductionOrder.Repositories;
-using JewelryProductionOrder.Repositories.IRepository;
+﻿using System.Reflection.Metadata;
+using Azure.Core;
+using JewelryProductionOrder.Models;
 using Microsoft.AspNetCore.Mvc;
+using Models.Repositories.Repository.IRepository;
 
 public class DashboardController : Controller
 {
-    private readonly QuotationRequestRepository _quotationRequestRepository; //To sum up for total revenue
-
-    public DashboardController(QuotationRequestRepository quotationRequestRepository)
-    {
-        _quotationRequestRepository = quotationRequestRepository;
-    }
-
-    //Find quotation request with status = approved and sum total price together
-    public IActionResult GetTotalRevenue()
-    {
-        var approvedQuotationRequests = _quotationRequestRepository.GetApprovedQuotationRequests();
-        var totalRevenue = approvedQuotationRequests.Sum(qr => qr.TotalPrice);
-
-        return Ok(new { TotalRevenue = totalRevenue });
-    }
-
+	private readonly IUnitOfWork _unitOfWork;
+	public DashboardController(IUnitOfWork unitOfWork)
+	{
+		_unitOfWork = unitOfWork;
+	}
     public IActionResult Index()
     {
-        return View();
+        List<QuotationRequest> requests = _unitOfWork.QuotationRequest.GetAll()
+           .Where(qr => qr.Status == "Approved")
+           .ToList();
+
+        List<string> rDates = requests.Select(qr => qr.CreatedAt.ToString("yyyy-MM-dd")).Distinct().ToList();
+        List<decimal> revenues = rDates.Select(date => requests.Where(qr => qr.CreatedAt.ToString("yyyy-MM-dd") == date).Sum(qr => qr.TotalPrice ?? 0)).ToList();
+
+        ViewBag.Dates = rDates;
+        ViewBag.Revenues = revenues;
+
+        return View(requests);
     }
+
+    //public IActionResult RevenuePeriod(DateTime? startDate, DateTime? endDate)
+    //{
+    //    List<QuotationRequest> data;
+    //    if (startDate.HasValue && endDate.HasValue)
+    //    {
+    //        if (startDate > endDate)
+    //        {
+    //            data = _unitOfWork.QuotationRequest.GetAll()
+    //            .Where(qr => qr.Status == "Approved" && qr.CreatedAt>= startDate && qr.CreatedAt <= endDate)
+    //            .ToList();
+    //            List<string> dates = data.Select(qr => qr.CreatedAt.ToString("yyyy-MM-dd")).Distinct().ToList();
+    //            List<decimal> revenues = dates.Select(date => data.Where(qr => qr.CreatedAt.ToString("yyyy-MM-dd") == date).Sum(qr => qr.TotalPrice ?? 0)).ToList();
+
+    //            ViewBag.Dates = dates;
+    //            ViewBag.Revenues = revenues;
+
+    //            return View(data);
+    //        }
+    //    }
+    //    else
+    //    {
+    //        return RedirectToAction("Index");
+    //    }
+    //    return View();
+    //}
 }
