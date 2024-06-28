cbc88c5e835994c42092fd5ea520f08d66bc1564
WMumei <60645955+WMumei@users.noreply.github.com>
2024-06-28 16:47:54 +0700

    feat: add ShoppingCart model, ProductionRequestDetail, modify UI

---
+++ JewelryProductionOrder/Controllers/ShoppingCartController.cs
@@ -0,0 +1,36 @@
+﻿using JewelryProductionOrder.Data;
+using JewelryProductionOrder.Models;
+using JewelryProductionOrder.Models.ViewModels;
+using JewelryProductionOrder.Repositories.IRepository;
+using Microsoft.AspNetCore.Mvc;
+using Models.Repositories.Repository.IRepository;
+using System.Security.Claims;
+
+namespace JewelryProductionOrder.Controllers
+{
+    public class ShoppingCartController : Controller
+    {
+		private readonly IUnitOfWork _unitOfWork;
+		public ShoppingCartController(IUnitOfWork unitOfWork)
+		{
+			_unitOfWork = unitOfWork;
+		}
+
+        public IActionResult Checkout()
+        {
+			var claimsIdentity = (ClaimsIdentity)User.Identity;
+			var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+			// TODO: handle filling production req
+			OrderVM order = new()
+			{
+				ShoppingCarts = _unitOfWork.ShoppingCart.GetAll(u => u.UserId == userId,
+				includeProperties: "BaseDesign").ToList(),
+				ProductionRequest = new()
+			};
+
+
+			return View(order);
+		}
+
+    }
+}
---
+++ JewelryProductionOrder/Models/BaseDesign.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JewelryProductionOrder.Models
+{
+    public class BaseDesign
+    {
+        [Key]
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Image { get; set; }
+        //public string? DesignFile { get; set; }
+        public string? Status { get; set; }
+
+    }
+}
