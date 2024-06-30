e25d143313a65e26657dd229ae42c62ada88e6f5
HoaNguyen1903 <hoanhtse185071@gmail.com>
2024-06-30 18:51:46 +0700

    added date time filter (not completed)

--- JewelryProductionOrder/Controllers/DashboardController.cs
+++ JewelryProductionOrder/Controllers/DashboardController.cs
@@ -55,6 +55,53 @@ public class DashboardController : Controller
             return View(requests);
         }
     }
+
+    [HttpPost]
+    public async Task<ActionResult> Index(DateTime? startDate, DateTime? endDate)
+    {
+        {
+            var role = await _roleManager.FindByNameAsync("Customer");
+            if (role == null)
+            {
+                return NotFound("Role not found");
+            }
+            var userIds = await _userManager.GetUsersInRoleAsync("Customer");
+            if (userIds == null)
+            {
+                return BadRequest("Error retrieving users in role 'Customer'");
+            }
+            int customerCount = userIds.Count;
+
+            List<QuotationRequest> requests = _unitOfWork.QuotationRequest.GetAll()
+           .Where(qr => qr.Status == "Approved")
+           .ToList();
+            if (startDate < endDate)
+            {
+                if (startDate.HasValue && endDate.HasValue)
+                {
+                    requests = requests.Where(qr => qr.CreatedAt >= startDate.Value && qr.CreatedAt <= endDate.Value).ToList();
+                }
+            }
+            //list<delivery> deliveries = _unitofwork.delivery.getall().tolist();
+
+            List<string> rDates = requests.Select(qr => qr.CreatedAt.ToString("yyyy-MM-dd")).Distinct().ToList();
+            List<decimal> revenues = rDates.Select(date => requests.Where(qr => qr.CreatedAt.ToString("yyyy-MM-dd") == date).Sum(qr => qr.TotalPrice ?? 0)).ToList();
+
+            //List<string> dDates = deliveries.Where(d => d.DeliveredAt.HasValue)
+            //                                .Select(d => d.DeliveredAt.Value.ToString("yyyy-MM-dd"))
+            //                                .Distinct()
+            //                                .ToList();
+            //List<int> deliveryCounts = dDates.Select(date => deliveries.Where(d => d.DeliveredAt.HasValue && d.DeliveredAt.Value.ToString("yyyy-MM-dd") == date).Count()).ToList();
+
+            ViewBag.Dates = rDates;
+            ViewBag.Revenues = revenues;
+            ViewBag.CustomerCount = customerCount;
+            //ViewBag.DeliveryDates = dDates;
+            //ViewBag.DeliveryCounts = deliveryCounts;
+
+            return View(requests);
+        }
+    }
 }
 
     //public IActionResult RevenuePeriod(DateTime? startDate, DateTime? endDate)
