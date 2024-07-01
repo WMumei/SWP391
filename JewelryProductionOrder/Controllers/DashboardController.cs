33b5ef196e40cb8d083eb14b718972edb3cafd85
HoaNguyen1903 <hoanhtse185071@gmail.com>
2024-07-01 23:12:01 +0700

    Delete unnecessaries

--- JewelryProductionOrder/Controllers/DashboardController.cs
+++ JewelryProductionOrder/Controllers/DashboardController.cs
@@ -104,35 +104,3 @@ public class DashboardController : Controller
         }
     }
 }
-
-            List<QuotationRequest> requests = _unitOfWork.QuotationRequest.GetAll()
-           .Where(qr => qr.Status == "Approved")
-           .ToList();
-            if (startDate < endDate)
-            {
-                if (startDate.HasValue && endDate.HasValue)
-                {
-                    requests = requests.Where(qr => qr.CreatedAt >= startDate.Value && qr.CreatedAt <= endDate.Value).ToList();
-                }
-            }
-            //list<delivery> deliveries = _unitofwork.delivery.getall().tolist();
-
-            List<string> rDates = requests.Select(qr => qr.CreatedAt.ToString("yyyy-MM-dd")).Distinct().ToList();
-            List<decimal> revenues = rDates.Select(date => requests.Where(qr => qr.CreatedAt.ToString("yyyy-MM-dd") == date).Sum(qr => qr.TotalPrice ?? 0)).ToList();
-
-            //List<string> dDates = deliveries.Where(d => d.DeliveredAt.HasValue)
-            //                                .Select(d => d.DeliveredAt.Value.ToString("yyyy-MM-dd"))
-            //                                .Distinct()
-            //                                .ToList();
-            //List<int> deliveryCounts = dDates.Select(date => deliveries.Where(d => d.DeliveredAt.HasValue && d.DeliveredAt.Value.ToString("yyyy-MM-dd") == date).Count()).ToList();
-
-            ViewBag.Dates = rDates;
-            ViewBag.Revenues = revenues;
-            ViewBag.CustomerCount = customerCount;
-            //ViewBag.DeliveryDates = dDates;
-            //ViewBag.DeliveryCounts = deliveryCounts;
-
-            return View(requests);
-        }
-    }
-}
