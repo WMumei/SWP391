ad7c5bb188da3e405a68ad662e6545b22672d5ec
LHBaoSE185036 <baolhse185036@fpt.edu.vn>
2024-07-01 14:19:57 +0700

    Change post class

--- JewelryProductionOrder/Models/Post.cs
+++ JewelryProductionOrder/Models/Post.cs
@@ -7,6 +7,7 @@ namespace JewelryProductionOrder.Models
         public int Id { get; set; }
         public string Title { get; set; }
         public string Image { get; set; }
+        public string Content { get; set; }
         public DateTime CreatedAt { get; set; }
         public string SalesStaffId { get; set; }
         [ForeignKey("SalesStaffId")]
