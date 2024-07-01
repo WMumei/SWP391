8f6ba1118e3e9e306c90aba59ca0b095cda38ec4
Vũ Minh Đức <ducvmse185065@fpt.edu.vn>
2024-07-01 18:05:43 +0700

    add some constant strings to SD.cs

--- JewelryProductionOrder/Utility/SD.cs
+++ JewelryProductionOrder/Utility/SD.cs
@@ -8,5 +8,16 @@
         public const string Role_Customer = "Customer";
         public const string Role_Admin = "Admin";
         public const string Role_Manager = "Manager";
+
+        public const string StatusCancelled = "Cancelled";
+        public const string StatusProcessing = "Processing";
+		public const string StatusDelivered = "Delivered";
+
+        public const string StatusPending = "Pending";
+        public const string StatusDiscontinued = "Discontinued";
+        public const string CustomerApproved = "Approved by Customer";
+        public const string CustomerDisapproved = "Disapproved by Customer";
+		public const string ManagerApproved = "Approved by Manager";
+        public const string ManagerDisapproved = "Disapproved by Manager";
     }
 }
