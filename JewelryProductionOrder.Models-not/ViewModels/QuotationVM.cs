f7c9678b095bc36027501b780d3f10e1bead013a
WMumei <60645955+WMumei@users.noreply.github.com>
2024-07-21 13:27:15 +0700

    fix: Splitting projects bug

---
+++ JewelryProductionOrder.Models-not/ViewModels/QuotationVM.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace JewelryProductionOrder.Models.ViewModels
+{
+    public class QuotationVM
+    {
+        public QuotationRequest QuotationRequest { get; set; }
+        //public User SaleStaff { get; set; } //Checkoutform
+        public Jewelry Jewelry { get; set; }
+		
+		//public List<Jewelry> jewelries { get; } = [];
+		public MaterialSet MaterialSet { get; set; }
+		[ValidateNever]
+		public IEnumerable<SelectListItem> JewelryList { get; set; }
+	}
+}
---
+++ JewelryProductionOrder.Models/ViewModels/DashboardVM.cs
@@ -0,0 +1,8 @@
+﻿using JewelryProductionOrder.Models;
+
+public class DashboardVM
+{
+    public List<ProductionRequest> ProductionRequests { get; set; }
+    public List<Delivery> Deliveries { get; set; }
+    public List<QuotationRequest> QuotationRequests { get; set; }
+}
