d0e625ac390ba41339c06ab66b7cc13f38dcf314
HoaNguyen1903 <hoanhtse185071@gmail.com>
2024-07-22 23:44:58 +0700

    Added gemstone controller (not done yet) added button to layout, modified model gemstone

--- JewelryProductionOrder.Models/Gemstone.cs
+++ JewelryProductionOrder.Models/Gemstone.cs
@@ -1,15 +1,21 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿	using System.ComponentModel.DataAnnotations;
 
-namespace JewelryProductionOrder.Models
-{
-	public class Gemstone
+	namespace JewelryProductionOrder.Models
 	{
-		public int Id { get; set; }
-		public string Name { get; set; }
-		[Range(0, double.MaxValue)]
-		public decimal Price { get; set; }
-		[Range(0, double.MaxValue)]
-		public decimal Weight { get; set; }
-		public List<MaterialSet> MaterialSets { get; } = [];
+		public class Gemstone
+		{
+			public int Id { get; set; }
+			public string Name { get; set; }
+			[Range(0, double.MaxValue)]
+			public decimal Price { get; set; }
+			[Range(0, double.MaxValue)]
+			public decimal Weight { get; set; }
+			public decimal Carat { get; set; }
+			public string Color { get; set; }
+			public string Clarity { get; set; }
+			public string Cut { get; set; }
+			public string Status { get; set; }
+
+			public List<MaterialSet> MaterialSets { get; } = [];
+		}
 	}
-}
---
+++ JewelryProductionOrder/Controllers/GemstoneController.cs
@@ -0,0 +1,12 @@
+﻿using JewelryProductionOrder.Models;
+using Microsoft.AspNetCore.Mvc;
+using Models.Repositories.Repository.IRepository;
+
+namespace JewelryProductionOrder.Areas.Staff.Controllers
+{
+    public class GemstoneController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly GemstoneManager<Gemstone>  
+    }
+}
