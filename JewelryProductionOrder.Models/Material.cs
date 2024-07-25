78c24c516395148d5d37eefdb3b380cd43864614
WMumei <60645955+WMumei@users.noreply.github.com>
2024-07-25 22:44:32 +0700

    fix: Material Properties

--- JewelryProductionOrder.Models/Material.cs
+++ JewelryProductionOrder.Models/Material.cs
@@ -6,7 +6,9 @@ namespace JewelryProductionOrder.Models
 	{
 		public int Id { get; set; }
 		[StringLength(100)]
-		public string Name { get; set; }
+		public string Type { get; set; }
+		public string Purity { get; set; }
+		public string Color { get; set; }
 		[Range(0, double.MaxValue)]
 		public decimal Price { get; set; }
 		public List<MaterialSet> MaterialSets { get; } = [];
