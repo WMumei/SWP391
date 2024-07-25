ff60b02c5ba6a29baed9b65ad97632a964a3d24a
Vũ Minh Đức <ducvmse185065@fpt.edu.vn>
2024-07-25 19:27:18 +0700

    add authorize for baseDesign CRUD

--- JewelryProductionOrder/Controllers/BaseDesignController.cs
+++ JewelryProductionOrder/Controllers/BaseDesignController.cs
@@ -67,12 +67,14 @@ namespace JewelryProductionOrder.Controllers
 			return RedirectToAction("Index", "Home");
 		}
 
+		[Authorize(Roles = $"{SD.Role_Manager},{SD.Role_Design}")]
 		public IActionResult Index()
 		{
 			List<BaseDesign> designList = _unitOfWork.BaseDesign.GetAll().ToList();
 			return View(designList);
 		}
 
+		[Authorize(Roles = $"{SD.Role_Customer},{SD.Role_Design},{SD.Role_Manager}")]
 		public IActionResult ViewAll(int bId)
 		{
 			var baseDesign = _unitOfWork.BaseDesign.GetAll(jD => jD.Id == bId).ToList();
@@ -82,13 +84,14 @@ namespace JewelryProductionOrder.Controllers
 			return View(baseDesign);
 		}
 
+		[Authorize(Roles = $"{SD.Role_Customer},{SD.Role_Design},{SD.Role_Manager}")]
 		public IActionResult Details(int bId)
 		{
 			BaseDesign design = _unitOfWork.BaseDesign.Get(design => design.Id == bId);
 			return View(design);
 		}
 
-		[Authorize(Roles = $"{SD.Role_Design},{SD.Role_Manager}")]
+		[Authorize(Roles = $"{SD.Role_Customer},{SD.Role_Design}")]
 		public IActionResult Edit(int bId)
 		{
 			var baseDesign = _unitOfWork.BaseDesign.Get(m => m.Id == bId);
@@ -100,7 +103,7 @@ namespace JewelryProductionOrder.Controllers
 		}
 
 		[HttpPost]
-		[Authorize(Roles = $"{SD.Role_Design},{SD.Role_Manager}")]
+		[Authorize(Roles = $"{SD.Role_Customer},{SD.Role_Design}")]
 		public IActionResult Edit(BaseDesign baseDesign, IFormFile? file)
 		{
 			var existingDesign = _unitOfWork.BaseDesign.Get(d => d.Id == baseDesign.Id);
@@ -131,7 +134,7 @@ namespace JewelryProductionOrder.Controllers
 			return RedirectToAction("Index");
 		}
 
-		[Authorize(Roles = $"{SD.Role_Design},{SD.Role_Manager}")]
+		[Authorize(Roles = $"{SD.Role_Customer},{SD.Role_Design}")]
 		public IActionResult Delete(int bId)
 		{
 			BaseDesign baseDesign = _unitOfWork.BaseDesign.Get(bD => bD.Id == bId);
