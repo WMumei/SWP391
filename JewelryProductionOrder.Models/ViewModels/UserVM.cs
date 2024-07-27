97fcb2937a43cf53e41b9aadfdd662035b8674a4
HoaNguyen1903 <hoanhtse185071@gmail.com>
2024-07-27 23:32:47 +0700

    Creating new account for employee, can't take role and can't generate ID automatically (sucks)

--- JewelryProductionOrder.Models/ViewModels/UserVM.cs
+++ JewelryProductionOrder.Models/ViewModels/UserVM.cs
@@ -10,9 +10,11 @@ namespace JewelryProductionOrder.Models.ViewModels
         public string Id { get; set; }
         public string Name { get; set; }
         public string UserName { get; set; }
+        public string Password { get; set; }
         public string Email { get; set; }
         public DateTimeOffset? LockoutEnd { get; set; }
         public string Role { get; set; }
-	}
+        public SelectList RoleSelectList { get; set; }
+    }
 
 }
