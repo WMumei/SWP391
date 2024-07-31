eb7a9c93ade589fd157eafab78946beb2e0e964c
ngoxuanhanhnhi <hanhnhi10022005@gmail.com>
2024-07-31 17:43:08 +0700

    fix column name null in user table

--- JewelryProductionOrder/Controllers/WarrantyCardController.cs
+++ JewelryProductionOrder/Controllers/WarrantyCardController.cs
@@ -29,6 +29,9 @@ namespace JewelryProductionOrder.Controllers
 			ProductionRequest productionRequest = _unitOfWork.ProductionRequest.Get(j => j.Id == jewelry.ProductionRequestId);
 			var customer = _unitOfWork.User.Get(u => u.Id == productionRequest.CustomerId);
 			jewelry.CustomerId = customer.Id;
+			customer.Name= productionRequest.ContactName;
+			_unitOfWork.User.Update(customer);
+			_unitOfWork.Save();
             //if (jewelry.MaterialSet == null && jewelry.QuotationRequests == null)
             //{
             //	TempData["Error"] = "Please create Material Set and Quotation Request!"; 
@@ -67,7 +70,7 @@ namespace JewelryProductionOrder.Controllers
 				return RedirectToAction("RequestIndex", "Jewelry", new { reqId = jewelry.ProductionRequestId });
 			}
 
-			ProductionRequest productionRequest = _unitOfWork.ProductionRequest.Get(j => j.Id == jewelry.ProductionRequestId);
+			ProductionRequest productionRequest = _unitOfWork.ProductionRequest.Get(j => j.Id == jewelry.ProductionRequestId,includeProperties:"Jewelries",tracked:true);
 			var customer = _unitOfWork.User.Get(u => u.Id == productionRequest.CustomerId);
 
 
